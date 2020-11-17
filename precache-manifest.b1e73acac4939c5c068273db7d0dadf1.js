f63803dbb390f5d1ffeeaa36dc41da4d4d4bd479
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2020-11-17 17:01:58 +0100

    Updates

---
+++ precache-manifest.b1e73acac4939c5c068273db7d0dadf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72ac644934db6a250ab536ff981bcc4",
+    "url": "/whisky/index.html"
+  },
+  {
+    "revision": "84bd3d37e73c72e19a7c",
+    "url": "/whisky/static/css/main.4874997d.chunk.css"
+  },
+  {
+    "revision": "bf4d4280099e8c3c1482",
+    "url": "/whisky/static/js/2.15adbae1.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/whisky/static/js/2.15adbae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84bd3d37e73c72e19a7c",
+    "url": "/whisky/static/js/main.846975a5.chunk.js"
+  },
+  {
+    "revision": "2cb66ebfca4802a89c9a",
+    "url": "/whisky/static/js/runtime-main.d0106814.js"
+  },
+  {
+    "revision": "505dce53efcf88edce7688c08d2c06a8",
+    "url": "/whisky/static/media/placeholder.505dce53.jpeg"
+  }
+]);
