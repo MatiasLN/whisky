d14d6042242bd3f208a540a98b353ec7ec04c789
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2020-11-29 15:18:13 +0100

    added titles array

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import { projectFirestore } from "../firebase/config";
-import useStorage from "./useStorage";
 
 const useFirestore = (collection) => {
   const [docs, setDocs] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [titles, setTitles] = useState([]);
 
   useEffect(() => {
     const unsub = projectFirestore
@@ -12,19 +12,20 @@ const useFirestore = (collection) => {
       .orderBy("createdAt", "desc")
       .onSnapshot((snap) => {
         let documents = [];
+        let titles = [];
         snap.forEach((doc) => {
+          titles.push(doc.data().title);
           documents.push({ ...doc.data(), id: doc.id });
         });
         setDocs(documents);
         setLoading(false);
+        setTitles(titles);
       });
-
     return () => unsub();
     // this is a cleanup function that react will run when
     // a component using the hook unmounts
   }, [collection]);
-
-  return { docs, loading };
+  return { docs, loading, titles };
 };
 
 export default useFirestore;
