8a7eba07a6b53f05ee28e01bac763b4a3736bb41
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2020-12-03 01:30:01 +0100

    added proper ref

--- src/comps/WhiskyItem/WhiskyItem.js
+++ src/comps/WhiskyItem/WhiskyItem.js
@@ -13,16 +13,15 @@ const ImageItem = () => {
   const [rating, setRating] = useState("");
   const [notes, setNotes] = useState("");
   const [url, setUrl] = useState("");
+  const [title, setTitle] = useState("");
 
   const user = useContext(UserContext);
   const { state } = useContext(WhiskyContext);
-
-  const LOCAL_DOMAINS = ["localhost", "127.0.0.1"];
   const uid = user.user.uid;
   let whiskyName = state.whisky.split(" ").join("_");
 
-  const collectionRef = projectFirestore.collection(uid).doc(state.id);
   useEffect(() => {
+    const collectionRef = projectFirestore.collection(uid).doc(state.id);
     collectionRef
       .get()
       .then((doc) => {
@@ -30,42 +29,21 @@ const ImageItem = () => {
         setRating(doc.data().star);
         setNotes(doc.data().notes);
         setUrl(doc.data().url);
+        setTitle(doc.data().title);
       })
       .catch(function (error) {
         console.log("Error getting document:", error);
       });
-
-    // if (LOCAL_DOMAINS.includes(window.location.hostname)) {
-    //   setData({
-    //     createdAt: { seconds: 1606776658, nanoseconds: 177000000 },
-    //     notes: "Frukt, nøtter og lang avslutning av citrus ",
-    //     star: "7",
-    //     title: "Glenfarclas 21",
-    //     url:
-    //       "https://firebasestorage.googleapis.com/v0/b/whisky-c2f56.appspot.com/o/67E0C835-0F90-4F9E-A9F8-5D4059A050D9.jpeg?alt=media&token=176ec1e8-1ead-456a-91b4-6a1912b967ae",
-    //   });
-    // } else {
-    //   collectionRef
-    //     .get()
-    //     .then((doc) => {
-    //       setData(doc.data());
-    //       setRating(doc.data().star);
-    //       setNotes(doc.data().notes);
-    //       setUrl(doc.data().url);
-    //       console.log(doc.data());
-    //     })
-    //     .catch(function (error) {
-    //       console.log("Error getting document:", error);
-    //     });
-    // }
   }, []);
 
   const handleSetRating = (rating) => {
+    const collectionRef = projectFirestore.collection(uid).doc(state.id);
     setRating(rating);
     collectionRef.update({ star: rating });
   };
 
   const changeHandlerTextarea = (e) => {
+    const collectionRef = projectFirestore.collection(uid).doc(state.id);
     setNotes(e.target.value);
     collectionRef.update({ notes: notes });
   };
@@ -78,16 +56,16 @@ const ImageItem = () => {
     <>
       <div className="whiskyItem">
         <div className="image" key={state.id} onClick={handleModal}>
-          <Image data={data.url} />
+          <Image data={url} />
         </div>
-        <h2>{data.title}</h2>
+        <h2>{title}</h2>
         <div className="notes">
           <textarea
             id="file-notes"
             rows={5}
             cols={5}
             placeholder="Smaksnotater ..."
-            value={data.notes}
+            value={notes}
             onChange={changeHandlerTextarea}
           />
         </div>
