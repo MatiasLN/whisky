1c194e4822e36b23a65dcce5bf4b9aedf4975be4
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2020-12-03 01:32:33 +0100

    use local data instead of calling db if there is any

--- src/comps/WhiskyData/WhiskyData.js
+++ src/comps/WhiskyData/WhiskyData.js
@@ -4,40 +4,75 @@ import GetWhiskyData from "./GetWhiskyData";
 import { useFetch } from "../../api/useFetch";
 
 export default ({ title }) => {
-  const { data, loading } = useFetch(title);
-  if (title && data.length) {
+  let name = localStorage.getItem("polet_name");
+  let key = localStorage.getItem("polet_name");
+  let productID = localStorage.getItem("polet_productID");
+  let percentage = localStorage.getItem("polet_percentage");
+  let country = localStorage.getItem("polet_country");
+  let region = localStorage.getItem("polet_region");
+  let destilery = localStorage.getItem("polet_destilery");
+  let descColour = localStorage.getItem("polet_descColour");
+  let descTaste = localStorage.getItem("polet_descTaste");
+  let descOdour = localStorage.getItem("polet_descOdour");
+  if (name) {
     return (
       <>
-        {loading ? (
-          <h2 className="loadingData">Laster inn data fra Vinmonopolet ...</h2>
-        ) : (
-          <div className="whiskyDetailsContainer">
-            {data &&
-              data.map((data) => (
-                <WhiskyDetails
-                  key={data.basic.productId}
-                  productID={data.basic.productId}
-                  name={data.basic.productLongName}
-                  alcohol={data.basic.alcoholContent}
-                  price={data.prices[0].salesPrice}
-                  country={data.origins.origin.country}
-                  region={data.origins.origin.region}
-                  destilery={data.logistics.manufacturerName}
-                  descColour={data.description.characteristics.colour}
-                  descOdour={data.description.characteristics.odour}
-                  descTaste={data.description.characteristics.taste}
-                />
-              ))}
-            <GetWhiskyData />
-          </div>
-        )}
+        <div className="whiskyDetailsContainer">
+          <WhiskyDetails
+            key={key}
+            productID={productID}
+            name={name}
+            alcohol={percentage}
+            price={name}
+            country={country}
+            region={region}
+            destilery={destilery}
+            descColour={descColour}
+            descOdour={descTaste}
+            descTaste={descOdour}
+          />
+          <GetWhiskyData />
+        </div>
       </>
     );
   } else {
-    return (
-      <>
-        <GetWhiskyData notFound />
-      </>
-    );
+    const { data, loading } = useFetch(title);
+    if (title && data.length) {
+      return (
+        <>
+          {loading ? (
+            <h2 className="loadingData">
+              Laster inn data fra Vinmonopolet ...
+            </h2>
+          ) : (
+            <div className="whiskyDetailsContainer">
+              {data &&
+                data.map((data) => (
+                  <WhiskyDetails
+                    key={data.basic.productId}
+                    productID={data.basic.productId}
+                    name={data.basic.productLongName}
+                    alcohol={data.basic.alcoholContent}
+                    price={data.prices[0].salesPrice}
+                    country={data.origins.origin.country}
+                    region={data.origins.origin.region}
+                    destilery={data.logistics.manufacturerName}
+                    descColour={data.description.characteristics.colour}
+                    descOdour={data.description.characteristics.odour}
+                    descTaste={data.description.characteristics.taste}
+                  />
+                ))}
+              <GetWhiskyData />
+            </div>
+          )}
+        </>
+      );
+    } else {
+      return (
+        <>
+          <GetWhiskyData notFound />
+        </>
+      );
+    }
   }
 };
