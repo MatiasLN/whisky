b95ac3edcb082f0c7d76a5fb7c0271b7f9a06891
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2021-08-27 14:37:35 +0200

    updated build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ec77adcfbd6ad7f3f8239fc84e15a88d.js"
+  "./precache-manifest.cac4360d2147a14b94447b9e7ea683ae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
