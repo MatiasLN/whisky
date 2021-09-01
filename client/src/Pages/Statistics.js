85f8a83a566ab35e1bc83d6166ff2db75310bf49
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2021-09-01 23:32:35 +0200

    updated typos

--- client/src/Pages/Statistics.js
+++ client/src/Pages/Statistics.js
@@ -7,49 +7,50 @@ const StatisticsPage = () => {
   const [percentages, setPercentages] = useState([]);
   const [averageProcentage, setAverageProcentage] = useState(null);
   const [countries, setCountries] = useState([]);
+  const [destilery, setDestilery] = useState([]);
   const [stars, setStars] = useState([]);
   const [titles, setTitles] = useState([null]);
 
   useEffect(() => {
     const getData = async () => {
       const titles = [];
+      let total = 0;
       const response = db.collection(uid);
+
       const data = await response.get();
       data.docs.forEach((item) => {
         titles.push(item.data().title);
-
         regions[item.data().polet_region] =
           (regions[item.data().polet_region] || 0) + 1;
-
+        destilery[item.data().polet_destilery] =
+          (destilery[item.data().polet_destilery] || 0) + 1;
         countries[item.data().polet_country] =
           (countries[item.data().polet_country] || 0) + 1;
-
         stars[item.data().star] = (stars[item.data().star] || 0) + 1;
 
         // add percantages to array and get them ready for avg calculation
-        var myNumber = "";
-        myNumber = item.data().polet_percentage;
-        var dec = myNumber - Math.floor(myNumber);
-        myNumber = myNumber - dec;
-        var formattedNumber = ("0" + myNumber).slice(-2);
+        var number = "";
+        number = item.data().polet_percentage;
+        var dec = number - Math.floor(number);
+        number = number - dec;
+        var formattedNumber = ("0" + number).slice(-2);
         if (formattedNumber != "aN") {
           percentages.push(parseInt(formattedNumber));
         }
       });
 
-      setCountries(countries);
-      setStars(stars);
-      setPercentages(percentages);
-      setRegions(regions);
-      setTitles(titles);
-
       // calculate average of %
-      var total = 0;
-      for (var i = 0; i < percentages.length; i++) {
+      for (let i = 0; i < percentages.length; i++) {
         total += percentages[i];
       }
-      var averageProcentage = total / percentages.length;
+      const averageProcentage = total / percentages.length;
       setAverageProcentage(averageProcentage);
+
+      //   setCountries(countries);
+      setStars(stars);
+      //   setPercentages(percentages);
+      //   setRegions(regions);
+      setTitles(titles);
     };
 
     getData();
@@ -62,6 +63,18 @@ const StatisticsPage = () => {
           Du har smakt <strong>{titles.length}</strong> ulike whiskyer
         </p>
       ) : null}
+
+      <h2>Destilleri</h2>
+      {regions ? (
+        <ul>
+          {Object.entries(destilery).map(([destilery, value]) => (
+            <li key={destilery}>
+              <strong>{destilery}</strong>: {value}
+            </li>
+          ))}
+        </ul>
+      ) : null}
+
       <h2>Distrikter</h2>
       {regions ? (
         <ul>
