27136b2106db5ac2b7e3bbd2a3ffa0748ea58d0a
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2021-09-02 15:20:07 +0200

    updated navigation structure

--- client/src/comps/Navigation/Navigation.js
+++ client/src/comps/Navigation/Navigation.js
@@ -1,17 +1,45 @@
 import React from "react";
+import { logOut } from "../../firebase/config";
+import { Link } from "react-router-dom";
 import useUser from "../../hooks/useUser";
 
 const Navigation = () => {
-  const { photoURL, displayName } = useUser();
+  const { uid, photoURL, displayName } = useUser();
 
-  const handleNavigation = () => {
-    document.querySelector(".userPage").classList.toggle("show");
+  const handleClose = () => {
+    document.querySelector(".mainMenuContainer").classList.toggle("show");
   };
 
   return (
-    <nav>
-      <img src={photoURL} alt={displayName} onClick={handleNavigation} />
-    </nav>
+    <>
+      {uid ? (
+        <>
+          <div className="mainMenuContainer">
+            <div className="mainMenuContent">
+              <button className="closeNav" onClick={handleClose}>
+                Lukk
+              </button>
+              <img src={photoURL} alt={displayName} />
+              <p>Hei {displayName}</p>
+              <nav className="mainMenu">
+                <Link to="/" className="active">
+                  Hovedside
+                </Link>
+                <Link to="/stats">Statistikk</Link>
+              </nav>
+              <button
+                className="logOutBtn"
+                onClick={() => {
+                  logOut();
+                }}
+              >
+                Logg ut
+              </button>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
   );
 };
 
