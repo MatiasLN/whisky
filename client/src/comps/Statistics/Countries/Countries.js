a5014d26e1878c90fc2e7774a70cc06ddfa71641
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2021-09-06 20:30:32 +0200

    created statistics components

---
+++ client/src/comps/Statistics/Countries/Countries.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+import useFirestore from "../../../hooks/useFirestore";
+import { Pie } from "react-chartjs-2";
+
+const Countries = () => {
+  const { docs } = useFirestore();
+  const [countries] = useState([]);
+  const [countryCount, setCountryCount] = useState(null);
+  const [name] = useState([]);
+  const [amount] = useState([]);
+
+  const options = {
+    plugins: {
+      legend: {
+        position: "right",
+      },
+    },
+  };
+
+  const data = {
+    labels: name,
+    datasets: [
+      {
+        label: "Antall flasker",
+        data: amount,
+        backgroundColor: [
+          "rgba(254, 200, 154)",
+          "rgba(255, 215, 186)",
+          "rgba(255, 229, 217)",
+          "rgba(254, 197, 187)",
+          "rgba(252, 213, 206)",
+          "rgba(250, 225, 221)",
+          "rgba(248, 237, 235)",
+          "rgba(232, 232, 228)",
+          "rgba(216, 226, 220)",
+          "rgba(236, 228, 219)",
+        ],
+        borderColor: [
+          "rgba(254, 200, 154)",
+          "rgba(255, 215, 186)",
+          "rgba(255, 229, 217)",
+          "rgba(254, 197, 187)",
+          "rgba(252, 213, 206)",
+          "rgba(250, 225, 221)",
+          "rgba(248, 237, 235)",
+          "rgba(232, 232, 228)",
+          "rgba(216, 226, 220)",
+          "rgba(236, 228, 219)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  function count(value, set) {
+    var total = 0;
+    Object.entries(value).map(([]) => (total += 1));
+    set(total);
+  }
+
+  useEffect(() => {
+    docs.map((item) => {
+      countries[item.polet_country] = (countries[item.polet_country] || 0) + 1;
+    });
+    count(countries, setCountryCount);
+
+    Object.entries(countries).map(
+      ([country, value]) => name.push(country) && amount.push(value)
+    );
+  }, [docs]);
+
+  return (
+    <>
+      <div className="chartContainer">
+        <div className="chart countryChart">
+          <h2>
+            <strong>{countryCount}</strong>land
+          </h2>
+          <Pie data={data} options={options} />
+        </div>
+      </div>
+      <div className="countriesContainer">
+        <h2>{countryCount} Land</h2>
+        {countries ? (
+          <ul>
+            {Object.entries(countries).map(([country, value]) => (
+              <li key={country}>
+                <p>{country}</p> <span>{value}</span>
+              </li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
+    </>
+  );
+};
+
+export default Countries;
---
+++ client/src/comps/Statistics/Regions/Regions.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from "react";
+import useFirestore from "../../../hooks/useFirestore";
+import { Bar } from "react-chartjs-2";
+
+const Regions = () => {
+  const { docs } = useFirestore();
+  const [Regions] = useState([]);
+  const [regCount, setregCount] = useState(null);
+  const [name] = useState([]);
+  const [amount] = useState([]);
+
+  const options = {
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+  };
+
+  const data = {
+    labels: name,
+    datasets: [
+      {
+        label: "Antall flasker",
+        data: amount,
+        backgroundColor: [
+          "rgba(254, 197, 187)",
+          "rgba(252, 213, 206)",
+          "rgba(250, 225, 221)",
+          "rgba(248, 237, 235)",
+          "rgba(232, 232, 228)",
+          "rgba(216, 226, 220)",
+          "rgba(236, 228, 219)",
+          "rgba(255, 229, 217)",
+          "rgba(255, 215, 186)",
+          "rgba(254, 200, 154)",
+        ],
+        borderColor: [
+          "rgba(254, 197, 187)",
+          "rgba(252, 213, 206)",
+          "rgba(250, 225, 221)",
+          "rgba(248, 237, 235)",
+          "rgba(232, 232, 228)",
+          "rgba(216, 226, 220)",
+          "rgba(236, 228, 219)",
+          "rgba(255, 229, 217)",
+          "rgba(255, 215, 186)",
+          "rgba(254, 200, 154)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  function count(value, set) {
+    var total = 0;
+    Object.entries(value).map(([]) => (total += 1));
+    set(total);
+  }
+
+  useEffect(() => {
+    docs.map((item) => {
+      Regions[item.polet_region] = (Regions[item.polet_region] || 0) + 1;
+    });
+    count(Regions, setregCount);
+
+    Object.entries(Regions).map(
+      ([region, value]) => name.push(region) && amount.push(value)
+    );
+  }, [docs]);
+
+  return (
+    <>
+      <div className="chartContainer">
+        <div className="chart">
+          <h2>
+            <strong>{regCount}</strong>regioner
+          </h2>
+          <Bar data={data} options={options} />
+        </div>
+      </div>
+      <div className="districtsContainer">
+        <h2>{regCount} Distriker</h2>
+        {Regions ? (
+          <ul>
+            {Object.entries(Regions).map(([region, value]) => (
+              <li key={region}>
+                <p>{region}</p> <span>{value}</span>
+              </li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
+    </>
+  );
+};
+
+export default Regions;
