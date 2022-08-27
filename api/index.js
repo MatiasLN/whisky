81e71235e5fcd575a19ed113873b771bf3988d10
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2022-08-27 18:16:22 +0200

    trying to fix vercel

--- api/index.js
+++ api/index.js
@@ -43,9 +43,9 @@ app.post("/api/search", (request, response) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname + "/client/build/index.html"));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
